3bafcbe42e237e3e4e3ee2617b90fdc7931ba740
Pedro <pedrobiqua@gmail.com>
2022-08-10 21:56:11 -0300

    Melhorias

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,23 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Instanciando o grafo
         Grafo grafo = new Grafo(5);
-        grafo.cria_adjacencia(4, 3, 2);
-        grafo.cria_adjacencia(4, 2, 9);
+
+        //Criando pontos de adjacencia
+        grafo.cria_adjacencia(2, 1, 2);
+        grafo.cria_adjacencia(3, 3, 5);
+        grafo.cria_adjacencia(3, 0, 9);
+        grafo.cria_adjacencia(1, 1, 5);
+        grafo.cria_adjacencia(4, 1, 5);
         grafo.imprime_adjacencias();
 
-        System.out.printf("Total adjacencias na linha %d: %d\n", 4,grafo.adjacentes(4));
-        System.out.printf("Total adjacencias na linha %d: %d\n", 2,grafo.adjacentes(2));
+        System.out.println("Adjacencias na linha "+ 0 + ": " + grafo.adjacentes(0));
+        System.out.println("Adjacencias na linha "+ 1 + ": " + grafo.adjacentes(1));
+        System.out.println("Adjacencias na linha "+ 2 + ": " + grafo.adjacentes(2));
+        System.out.println("Adjacencias na linha "+ 3 + ": " + grafo.adjacentes(3));
+        System.out.println("Adjacencias na linha "+ 4 + ": " + grafo.adjacentes(4) + "\n\n");
+        
     }
 
 }
